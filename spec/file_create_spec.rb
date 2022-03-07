ec74582f88764f9cc334ce028e5ec87a2ddac3f9
Rin Fabian <pfabianschool@gmail.com>
2022-03-07 17:14:02 -0600

    test; added a test to check for translation of a single letter to braille

--- spec/file_create_spec.rb
+++ spec/file_create_spec.rb
@@ -3,9 +3,9 @@ require 'tempfile'
 
 RSpec.describe FileCreate do
 
-  test_in_file = Tempfile.new('message.txt')
-  test_in_file.write('hello world')
-  let(:arguements){[test_in_file, 'braille.txt']}
+  test_in_file1 = Tempfile.new('message.txt')
+  test_in_file1.write('a')
+  let(:arguements){[test_in_file1, 'braille.txt']}
 
   before (:each) do
     @file = FileCreate.new(arguements)
@@ -19,4 +19,7 @@ RSpec.describe FileCreate do
     expect(@file.create).to be_a(File)
   end
 
+  it 'can translate a letter' do
+    expect(@file.tanslate).to eq('O . \n. . \n. . ')
+  end
 end
