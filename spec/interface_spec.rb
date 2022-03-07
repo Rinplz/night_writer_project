56b21d12f21b7f44ae81039c9a8f2d25a41df4d2
Rin Fabian <pfabianschool@gmail.com>
2022-03-07 12:46:21 -0600

    test; adjusted test to account for file name input

--- spec/interface_spec.rb
+++ spec/interface_spec.rb
@@ -3,10 +3,12 @@ require './lib/interface'
 RSpec.describe Interface do
 
   before (:each) do
-    interface = Interface.new
+    @interface1 = Interface.new
+    @interface2 = Interface.new
   end
 
   it 'can print' do
-    expect(interface.creation_message).to eq("Created 'braille.txt' containing 256 characters")
+    expect(@interface1.creation_message).to eq("Created 'braille.txt' containing 256 characters")
+    expect(@interface2.creation_message).to eq("Created 'braille.txt' containing 256 characters")
   end
 end
