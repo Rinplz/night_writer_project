e5efc7d4bf756dec3f7a4bab9cb9a2259408ece9
Rin Fabian <pfabianschool@gmail.com>
2022-03-07 20:23:41 -0600

    #format_for_roman

--- spec/translator_spec.rb
+++ spec/translator_spec.rb
@@ -8,16 +8,20 @@ RSpec.describe Translator do
     @translated_string3 = Translator.new("the quick brown fox jumps over the lazy dog")
   end
 
-  it 'can translate 1 letter' do
+  it 'can translate 1 letter to braille' do
     expect(@translated_string1.translate_to_braille).to eq("0.\n..\n..\n")
   end
 
-  it 'can translate more than 1 letter' do
+  it 'can translate more than 1 letter to braille' do
     expect(@translated_string2.translate_to_braille).to eq("0.0.0.0.0....00.0.0.00\n00.00.0..0..00.0000..0\n....0.0.0....00.0.0...\n")
   end
 
-  it 'can format an input' do
+  it 'can format an input for braille' do
     expect(@translated_string1.format_for_braille).to eq(['a'])
     expect(@translated_string3.format_for_braille).to eq(['the quick brown fox jumps over the lazy ', 'dog'])
+  end
+
+  it 'can format a single input for roman' do
+    expect(@translated_braille.format_for_roman).to eq(['0.\n..\n..\n'])
   end
 end
