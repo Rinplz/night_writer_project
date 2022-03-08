8cd04cf22a6cae3cdb4b04502d218c222232408c
Rin Fabian <pfabianschool@gmail.com>
2022-03-07 19:32:07 -0600

    feat; implemented #format into #translate to seperate characters at 40 lines

--- lib/translator.rb
+++ lib/translator.rb
@@ -8,11 +8,13 @@ class Translator
 
   def translate
     translated_string = ''
-    3.times do |count|
-      @to_translate.each_char do |character|
-        translated_string << @translations.translation[character][count]
-      end
+    format.each do |index|
+      3.times do |count|
+        index.each_char do |character|
+          translated_string << @translations.translation[character][count]
+        end
       translated_string << "\n"
+      end
     end
     translated_string
   end
